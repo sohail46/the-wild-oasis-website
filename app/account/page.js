47636d8201256e21da41f76a0d9fbe4081565501
Mohammed Sohail Shaikh <sohail28195@gmail.com>
2025-11-21 21:11:15 +0530

    added nested layout, app basic setup, with components and routes :sparkles:

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,11 @@
+export const metadata = {
+  title: 'Guest Area',
+};
+
 export default function Page() {
   return (
-    <div>
-      <h1>Account Page</h1>
-    </div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Sohail!
+    </h2>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,32 @@
-import Logo from '@/app/_components/Logo';
-import Navigation from '@/app/_components/Navigation';
-
 import '@/app/_styles/globals.css';
+import Header from '@/app/_components/Header';
+
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
-  title: 'The Wild Oasis',
+  title: {
+    template: '%s | The Wild Oasis',
+    default: 'Welcome | The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forests',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
