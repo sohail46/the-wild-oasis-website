428d19bbd0316fe5777ad82c58ce0bc72a6799f4
Mohammed Sohail Shaikh <sohail28195@gmail.com>
2025-11-30 19:42:21 +0530

    server and client component learnings :zap:

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,7 @@ import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 
 import { getCabin, getCabins } from '@/app/_lib/data-service';
+import TextExpander from '@/app/_components/TextExpander';
 
 // export const metadata = {
 //   title: 'Cabin Details',
@@ -44,7 +45,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
