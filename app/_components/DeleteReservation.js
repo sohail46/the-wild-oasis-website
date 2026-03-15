1fa05c5d23c15e698d2e394bec90878b8dba2d0e
Mohammed Sohail Shaikh <sohail28195@gmail.com>
2026-03-15 11:37:57 +0530

    feat: optimistic hook used for delete bookings :zap:

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -3,15 +3,14 @@
 import { useTransition } from 'react';
 import { TrashIcon } from '@heroicons/react/24/solid';
 
-import { deleteReservation } from '../_lib/actions';
 import SpinnerMini from './SpinnerMini';
 
-function DeleteReservation({ bookingId }) {
+function DeleteReservation({ bookingId, onDelete }) {
   const [isPending, startTransition] = useTransition();
 
   function handleDelete() {
     if (confirm('Are you sure you want to delete this reservation?')) {
-      startTransition(() => deleteReservation(bookingId));
+      startTransition(() => onDelete(bookingId));
     }
   }
 
